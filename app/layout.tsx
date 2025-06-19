305823f424bbbcf3140d9d5aa79b456b2063da6d
suifans <yueliao507@gmail.com>
2025-06-19 16:18:26 +0800

    Fix Clerk initialization error

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
       appearance={{
         baseTheme: undefined,
         variables: {
